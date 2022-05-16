d58b9f887f8c49f28027681c57ed9ca087668bd6
Malligos <butssergei7778@gmail.com>
2022-05-16 22:11:14 +0300

    Films

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-'use strict';
-const category = 'toys';
+const numberOfFilms = +prompt("Сколько фильмов Вы смотрели?" , ' ');
 
-console.log(`https://someurl.com/${category}/4`)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ' '),
+      b = prompt("На сколько Оцените его?"),
+      c = prompt("Один из последних просмотренных фильмов?", ' '),
+      d = prompt("На сколько Оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
