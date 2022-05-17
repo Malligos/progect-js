174e522f633879012206824e35ae62818d698b73
Malligos <butssergei7778@gmail.com>
2022-05-17 20:42:11 +0300

    *

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-const numberOfFilms = +prompt("Сколько фильмов Вы смотрели?" , ' ');
+let what = prompt('Назовите число!?');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat: false
-};
-
-const a = prompt("Один из последних просмотренных фильмов?", ' '),
-      b = prompt("На сколько Оцените его?"),
-      c = prompt("Один из последних просмотренных фильмов?", ' '),
-      d = prompt("На сколько Оцените его?");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+if( what < 49){
+    console.log('Error');
+} else if (what > 100) {
+    console.log('Big');
+} else {
+    console.log('Ok!');
+}
